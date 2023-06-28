0e5168cb69b3f3b70e2600b6ac69da99c4692927
wvclouds <138031353+wvclouds@users.noreply.github.com>
2023-06-29 02:17:07 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -42,10 +42,11 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-"protectedRoutes": [
+  "protectedRoutes": [
   "/Media/Movie",
-  "/🥟 Some test files/Protected route"
-],
+  "/Media/Series",
+  "/Media/Protected route"
+  ],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: 'mailto:wvclouds@gmail.com',
